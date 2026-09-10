4fd391bd761fd9abd336d12dd3c4b0db6f2e11ba
eduardagoncalves1 <eduardagon.ifc@gmail.com>
2026-09-10 08:53:30 -0300

    feat(produtos): adiciona model e migrations

---
+++ produtos/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+from .models import Produto
+
+
+@admin.register(Produto)
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ("id", "nome", "preco")
+    search_fields = ("nome",)
